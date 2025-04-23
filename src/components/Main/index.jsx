e70079dac6800e7e95d7acf772d5215c3f64d20b
ikram <ikramzaikou@gmail.com>
2025-04-24 00:09:07 +0100

    style

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,33 +1,29 @@
 import "./style.css";
-import image from "./image.jpg";
+import image from "./tokyo.jpg";
+import image2 from "./kyoto.jpg";
+import image3 from "./osaka.jpg";
 export default function Main() {
   return (
     <div className="main">
       <div className="card">
         <img src={image} alt="" className="card-imge" />
         <h2> tokyo </h2>
-        <p>hbidhchuihedhihedbihbbsdibidujh</p>
+        <p>Stroll under sakura canopies in Shinjuku Gyoen and along the Meguro River.</p>
+        <a href="#" className="read-more">Read More</a>
       </div>
       <div className="card">
-        <img src={image} alt="" className="card-imge" />
-        <h2> tokyo </h2>
-        <p>hbidhchuihedhihedbihbbsdibidujh</p>
+        <img src={image2} alt="" className="card-imge" />
+        <h2> Kyoto </h2>
+        <p>Sip matcha in bamboo-lined tea houses and wander through moss temples.</p>
+        <a href="#" className="read-more">Read More</a>
       </div>
       <div className="card">
-        <img src={image} alt="" className="card-imge" />
-        <h2> tokyo </h2>
-        <p>hbidhchuihedhihedbihbbsdibidujh</p>
+        <img src={image3} alt="" className="card-imge" />
+        <h2> Osaka </h2>
+        <p>Explore cherry-lit castles and pastel street food stalls in Dotonbori.</p>
+        <a href="#" className="read-more">Read More</a>
       </div>
-      <div className="card">
-        <img src={image} alt="" className="card-imge" />
-        <h2> tokyo </h2>
-        <p>hbidhchuihedhihedbihbbsdibidujh</p>
-      </div>
-      <div className="card">
-        <img src={image} alt="" className="card-imge" />
-        <h2> tokyo </h2>
-        <p>hbidhchuihedhihedbihbbsdibidujh</p>
-      </div>
+     
     </div>
   );
 }
