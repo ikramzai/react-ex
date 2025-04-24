9bc50e55c7acd567832f75e514a11f58a853e2aa
ikrame <ikramzaikou@gmail.com>
2025-04-24 17:24:32 +0100

    footer

---
+++ src/components/Footer/footercard/index.jsx
@@ -0,0 +1,30 @@
+export default function Footercard(props) {
+    return (
+       <div>
+        <h2>contact us</h2>
+        <ul className="social-list">
+          <li>
+            <a href="#">
+              <span>Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Instagram</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>LinkedIn</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      
+    );
+  }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,29 +1,10 @@
 import "./style.css";
+import Footercard from "./footercard";
+
 export default function Footer() {
   return (
     <div className="footerS">
-      <ul className="social-list">
-        <li>
-          <a href="#">
-            <span>Facebook</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span>Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span>Instagram</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span>LinkedIn</span>
-          </a>
-        </li>
-      </ul>
+     <Footercard/>
     </div>
   );
 }
